09b06d7e2bb463637cc04c4d587120edb63339fd
Gene Carter <gananda@gmail.com>
2020-12-15 15:36:02 -0500

    supporting rates and current

--- rnn_train_test.py
+++ rnn_train_test.py
@@ -7,10 +7,17 @@ import curbd
 dtData = 0.0641
 number_units = 255
 steps = 165
+trainType='currents'
 tData, xBump, hBump = curbd.bump_gen(number_units, dtData, steps)
 
-out = curbd.trainMultiRegionRNN(hBump, dtData, trainType='currents',
-                                verbose=True, dtFactor=10, nRunTrain=120,
+
+if trainType == 'currents':
+    activity = hBump
+else:
+    activity = xBump
+
+out = curbd.trainMultiRegionRNN(activity, dtData, trainType=trainType,
+                                verbose=True, dtFactor=100, nRunTrain=60,
                                 tauRNN=0.01, g=1.5, ampInWN=0.1, nRunFree=1,
                                 plotStatus=False)
 
@@ -18,7 +25,8 @@ mid = int(number_units/2)
 fig = pylab.figure()
 ax = fig.add_subplot(111)
 ax.plot(out['tRNN'], out['RNN'][mid, :], label='RNN')
-ax.plot(tData, hBump[mid, :], label='hBump', linestyle='dashed')
-ax.plot(tData, xBump[mid, :], label='xBump')
+ax.plot(tData, hBump[mid, :], label='current', linestyle='dashed')
+ax.plot(tData, xBump[mid, :], label='rate')
 ax.legend()
+ax.set_title('trained on {}'.format(trainType))
 fig.show()
