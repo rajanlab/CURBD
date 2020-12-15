36c7e8faa25e50146e54f3fd34cb2dbfe3bf70e4
Gene Carter <gananda@gmail.com>
2020-12-15 14:12:01 -0500

    improving rnn_train_test, assuming we model to currents

--- rnn_train_test.py
+++ rnn_train_test.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
+import numpy as np
+import pylab
+
 import curbd
 
 dtData = 0.0641
-number_units = 500
+number_units = 255
 steps = 165
 tData, xBump, hBump = curbd.bump_gen(number_units, dtData, steps)
 
-out = curbd.trainMultiRegionRNN(xBump, dtData, trainType='currents',
-                                verbose=True, dtFactor=100, nRunTrain=120,
+out = curbd.trainMultiRegionRNN(hBump, dtData, trainType='currents',
+                                verbose=True, dtFactor=10, nRunTrain=120,
                                 tauRNN=0.01, g=1.5, ampInWN=0.1, nRunFree=1,
-                                plotStatus=True)
+                                plotStatus=False)
+
+mid = int(number_units/2)
+fig = pylab.figure()
+ax = fig.add_subplot(111)
+ax.plot(out['tRNN'], out['RNN'][mid, :], label='RNN')
+ax.plot(tData, hBump[mid, :], label='hBump', linestyle='dashed')
+ax.plot(tData, xBump[mid, :], label='xBump')
+ax.legend()
+fig.show()
