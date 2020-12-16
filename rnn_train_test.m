73db832e8baf02b264b89df817a48b22db7b735c
Gene Carter <gananda@gmail.com>
2020-12-16 09:40:45 -0500

    cleaning up variable names

--- rnn_train_test.m
+++ rnn_train_test.m
@@ -2,9 +2,9 @@
 dtData = 0.0641
 number_units = 500
 steps = 165
-[tData, xBump, hBump] = bumpgen(number_units, dtData, steps);
+[tData, bump_rates, bump_current] = bumpgen(number_units, dtData, steps);
 
-model = trainMultiRegionRNN(xBump, struct( ...
+model = trainMultiRegionRNN(bump_rates, struct( ...
     'dtData', dtData, ...
     'dtFactor',10, ...
     'trainType','currents', ...
@@ -15,18 +15,18 @@ model = trainMultiRegionRNN(xBump, struct( ...
     'nRunFree',5));
 
 
-function [tData, xBump, hBump] = bumpgen(number_units, dt_data, steps)
-% generate a simple bump for testing trainMultiRegionRNN
+function [tData, bump_rates, bump_current] = bumpgen(number_units, dt_data, steps)
+% generate a simple bump for testing trainMultiRegionRNN, 
+% calculates current assuming the activation function is tanh
     tData = 0:dt_data:steps*dt_data;
-    xBump = zeros(number_units, length(tData));
+    bump_rates = zeros(number_units, length(tData));
     sig = 0.0343 * number_units; % scaled correctly in neuron space!!!
     norm_by = 2 * sig ^ 2;
     for i=1:number_units
-        xBump(i, :) = exp(-(i-number_units*tData/tData(end)).^2/(norm_by));
+        bump_rates(i, :) = exp(-(i-number_units*tData/tData(end)).^2/(norm_by));
     end
-%    hBump = log((xBump+0.01)./(1-xBump+0.01)); % current from rate
-    xBump = xBump/max(xBump, [], 'all');
-    xBump = min(xBump, 0.999);
-    xBump = max(xBump, -0.999);
-    hBump = atanh(xBump);
+    bump_rates = bump_rates/max(bump_rates, [], 'all');
+    bump_rates = min(bump_rates, 0.999);
+    bump_rates = max(bump_rates, -0.999);
+    bump_current = atanh(bump_rates);
 end
